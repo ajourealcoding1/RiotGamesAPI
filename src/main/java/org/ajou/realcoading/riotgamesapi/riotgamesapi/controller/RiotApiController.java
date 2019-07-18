4d81836483fb34ac8e392305a34f1ecfcd2dc0aa
doroddong <dongpang1014@ajou.ac.kr>
2019-07-19 02:02:11 +0900

    Add @RestController annotation in RiotApiController

--- src/main/java/org/ajou/realcoading/riotgamesapi/riotgamesapi/controller/RiotApiController.java
+++ src/main/java/org/ajou/realcoading/riotgamesapi/riotgamesapi/controller/RiotApiController.java
@@ -1,4 +1,7 @@
 package org.ajou.realcoading.riotgamesapi.riotgamesapi.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RiotApiController {
 }
