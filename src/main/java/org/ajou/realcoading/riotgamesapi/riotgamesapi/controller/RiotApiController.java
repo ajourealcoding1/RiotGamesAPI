8c48c161541477eba8bcbeaa15f6f8c6df545145
doroddong <dongpang1014@ajou.ac.kr>
2019-07-19 02:02:48 +0900

    Add @RequestMapping annotation in RiotApiController

--- src/main/java/org/ajou/realcoading/riotgamesapi/riotgamesapi/controller/RiotApiController.java
+++ src/main/java/org/ajou/realcoading/riotgamesapi/riotgamesapi/controller/RiotApiController.java
@@ -1,7 +1,9 @@
 package org.ajou.realcoading.riotgamesapi.riotgamesapi.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/RiotGamesApi")
 public class RiotApiController {
 }
