e27552c051ebe679a488d22794236f24f43757f0
JooJaeLin <jl1096@ajou.ac.kr>
2019-07-18 19:29:36 +0900

    first commit

---
+++ src/main/java/org/ajou/realcoading/riotgamesapi/riotgamesapi/RiotgamesApiApplication.java
@@ -0,0 +1,13 @@
+package org.ajou.realcoading.riotgamesapi.riotgamesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiotgamesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiotgamesApiApplication.class, args);
+	}
+
+}
