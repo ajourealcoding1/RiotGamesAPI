c86d71d2007eeec134ffe854fc93bd321ab48f54
doroddong <dongpang1014@ajou.ac.kr>
2019-07-19 02:09:43 +0900

    Make getAvailableSummonerName method in RiotApiController

--- src/main/java/org/ajou/realcoading/riotgamesapi/riotgamesapi/controller/RiotApiController.java
+++ src/main/java/org/ajou/realcoading/riotgamesapi/riotgamesapi/controller/RiotApiController.java
@@ -2,8 +2,12 @@ package org.ajou.realcoading.riotgamesapi.riotgamesapi.controller;
 
 import org.ajou.realcoading.riotgamesapi.riotgamesapi.service.SummonerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/RiotGamesApi")
@@ -11,4 +15,10 @@ public class RiotApiController {
 
     @Autowired
     private SummonerService summonerService;
+
+    @GetMapping("available-summonerName")
+    public List<String> getAvailableSummonerName() throws IOException{
+        return summonerService.getAvailableSummonerNames();
+    }
+
 }
