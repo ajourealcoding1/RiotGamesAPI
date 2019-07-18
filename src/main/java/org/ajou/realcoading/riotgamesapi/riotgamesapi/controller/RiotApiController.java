52cbe19f760ed0b748c509fbfd8825a15db57658
doroddong <dongpang1014@ajou.ac.kr>
2019-07-19 02:07:34 +0900

    Make SummonerService object with @Autowired annotation

--- src/main/java/org/ajou/realcoading/riotgamesapi/riotgamesapi/controller/RiotApiController.java
+++ src/main/java/org/ajou/realcoading/riotgamesapi/riotgamesapi/controller/RiotApiController.java
@@ -1,9 +1,14 @@
 package org.ajou.realcoading.riotgamesapi.riotgamesapi.controller;
 
+import org.ajou.realcoading.riotgamesapi.riotgamesapi.service.SummonerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/RiotGamesApi")
 public class RiotApiController {
+
+    @Autowired
+    private SummonerService summonerService;
 }
