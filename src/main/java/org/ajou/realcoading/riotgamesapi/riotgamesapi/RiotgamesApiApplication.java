d7fc0a4cb9ef893e6972173761a3ff4f5ff7a6ee
doroddong <dongpang1014@ajou.ac.kr>
2019-07-19 02:14:33 +0900

    Add @EnableScheduling annotation in RiotGamesApiApplication

--- src/main/java/org/ajou/realcoading/riotgamesapi/riotgamesapi/RiotgamesApiApplication.java
+++ src/main/java/org/ajou/realcoading/riotgamesapi/riotgamesapi/RiotgamesApiApplication.java
@@ -2,8 +2,10 @@ package org.ajou.realcoading.riotgamesapi.riotgamesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RiotgamesApiApplication {
 
 	public static void main(String[] args) {
