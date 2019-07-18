913304686efe32583d3d7fc98101f5f5aef64208
kimdohyun <kdh5998@ajou.ac.kr>
2019-07-18 20:51:11 +0900

    repository 내용 채웠습니다!!

--- src/main/java/org/ajou/realcoading/riotgamesapi/riotgamesapi/repository/SummonerGameGradeRepository.java
+++ src/main/java/org/ajou/realcoading/riotgamesapi/riotgamesapi/repository/SummonerGameGradeRepository.java
@@ -1,4 +1,26 @@
 package org.ajou.realcoading.riotgamesapi.riotgamesapi.repository;
 
+import org.ajou.realcoading.riotgamesapi.riotgamesapi.domain.SummonerGameGrade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class SummonerGameGradeRepository {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public SummonerGameGrade insertOrUpdatedSummnorGameGrade(SummonerGameGrade summonerGameGrade) {
+        return mongoTemplate.save(summonerGameGrade);
+    }
+
+    public SummonerGameGrade findGameGradeBySummnorName(String summonerName) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("summonerName").is(summonerName));
+
+        return mongoTemplate.findOne(query, SummonerGameGrade.class);
+    }
 }
