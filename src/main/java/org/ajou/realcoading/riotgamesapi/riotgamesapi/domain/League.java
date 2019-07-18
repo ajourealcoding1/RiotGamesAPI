90d829e9fa0aae98722433789ef8f01e6b0fd3b0
JooJaeLin <jl1096@ajou.ac.kr>
2019-07-18 19:40:20 +0900

    Add packages and domain Summoner and League

---
+++ src/main/java/org/ajou/realcoading/riotgamesapi/riotgamesapi/domain/League.java
@@ -0,0 +1,32 @@
+package org.ajou.realcoading.riotgamesapi.riotgamesapi.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
+public class League {
+
+    private String queueType;
+    @Id private String summonerName;
+    private boolean hostStreak;
+    private MiniSeriesDTO miniSeries;
+    private int wins;
+    private boolean veteran;
+    private int losses;
+    private String rank;
+    private String leagueId;
+    private boolean inactive;
+    private boolean freshBlood;
+    private String tier;
+    private String summonerId;
+    private int leaguePoints;
+
+    @Data
+    public static class MiniSeriesDTO{
+        private String progress;
+        private int losses;
+        private int target;
+        private int wins;
+    }
+
+}
